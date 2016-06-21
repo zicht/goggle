deda32d6b6e8512cfd5405a717c418d6c47f1360
Gerard van Helden <gerard@zicht.nl>
2016-06-21 14:47:36 +0200

    cs

--- bin/cfg.php
+++ bin/cfg.php
@@ -3,9 +3,7 @@
  * @author Gerard van Helden <gerard@zicht.nl>
  * @copyright Zicht Online <http://zicht.nl>
  */
-
-
-use \Zicht\ConfigTool\Application;
+use Zicht\ConfigTool\Application;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 (new Application())->run();
