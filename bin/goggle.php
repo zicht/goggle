7fba98a79e5f6b9017e6afedb370e9e79bf277f9
Gerard van Helden <gerard@zicht.nl>
2016-06-29 10:33:43 +0200

    add a check for autoload files

--- bin/goggle.php
+++ bin/goggle.php
@@ -4,6 +4,19 @@
  * @copyright Zicht Online <http://zicht.nl>
  */
 use Zicht\ConfigTool\Application;
-require_once __DIR__ . '/../vendor/autoload.php';
+
+$autoload = array_shift(
+    array_filter(
+        [__DIR__ . '/../vendor/autoload.php', __DIR__ . '/../../../autoload.php'], 
+        'is_file'
+    )
+);
+
+if (!$autoload) {
+    fprintf(STDERR, "No autoloader found.\n");
+    fprintf(STDERR, "You probably need to do a `composer install` within %s\n", realpath(__DIR__ . '/../'));
+    exit(250);
+}
+require_once $autoload;
 
 (new Application())->run();
