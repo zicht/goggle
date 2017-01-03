828b1c09ef05239b8225ab4adf2ee664d91cb564
Gerard van Helden <gerard@zicht.nl>
2017-01-03 20:21:02 +0100

    docs + fields

--- README.md
+++ README.md
@@ -24,14 +24,39 @@ A command line tool to easily read values from config files and output them in s
 ### Read a 'deep' value from a configuration file
 
 ```
-goggle get FILE element0..elementN [--format=FORMAT] [--each=e1 [--each e2 [...]]] [--property p1 [--property p2 [...]]]
+goggle get -i FILE element0..elementN [-o=FORMAT]
 ```
 
-Read value FILE from path `element0..elementN` and output it in the specified format. If `--each` is specified, iterate over that path. If `-p` is specified, filter those properties from the found objects.
+Read value FILE from path `element0..elementN` and output it in the specified format.
+ 
+### Process a set of values
+  
+```
+# Each of the records in the file FILE will be mapped by key `name`, and only fields
+# 'author' and 'name'  are kept, other fields are dropped.
+
+goggle process -i FILE mapBy name fields author name 
+```
 
 ## Example ##
 
+
 ### Example 1 ###
+Show package names, versions and authors, extracted from composer.lock
+
+```
+goggle get -i ./composer.lock packages | goggle process -t json fields name version authors | goggle process -t json mapBy name 
+```
+
+Or only get the one for `symfony/symfony`:
+
+```
+goggle get -i ./composer.lock packages | goggle process -t json fields name version authors | goggle process -t json mapBy name | goggle get "symfony/symfony"
+```
+
+
+
+### Example 2 ###
 Read the database host name from the following file and output it in JSON:
 
 ```
@@ -48,20 +73,20 @@ This would output:
 "foo"
 ```
 
-### Example 2 ###
+### Example 3 ###
 See all values available in a composer lock file
 
 ```
-goggle get -i composer.lock --format=dump
+goggle get -i composer.lock -o=dump
 ```
 
 Or read all package names and versions from a composer lockfile:
 
 ```
-goggle get -i composer.lock | goggle process packages -p name -p version
+goggle get -i composer.lock | goggle fields name version
 ``` 
 
-### Example 3 ###
+### Example 4 ###
 Or simply convert yml to json:
 
 ```
--- src/Zicht/ConfigTool/Command/ProcessCommand.php
+++ src/Zicht/ConfigTool/Command/ProcessCommand.php
@@ -24,11 +24,12 @@ class ProcessCommand extends IOCommand
             ->setName('process')
             ->setDescription("Process a data set recursively")
             ->setHelp(
-                'Each instruction is a certain operation on the data set'
+                'Each instruction is a certain operation on the data set, expecting a certain amount of arguments.' . PHP_EOL
+                . 'The amount of arguments is hard coded currently, per operation:' . PHP_EOL
+                . ' * map: get a property from the dataset (anologous to iter\map)' . PHP_EOL
+                . ' * '
             )
-            ->addArgument(
-                'instructions', Console\Input\InputArgument::IS_ARRAY, 'The item to read from the config, i.e. `parameters`'
-            );
+            ->addArgument('instructions', Console\Input\InputArgument::IS_ARRAY, 'The item to read from the config, i.e. `parameters`');
     }
 
     /**
@@ -48,6 +49,25 @@ class ProcessCommand extends IOCommand
         $data = iter\iterable($loader->load());
         while ($instruction = $shift()) {
             switch ($instruction) {
+                case 'fields':
+                    // eat up the rest of the arguments
+                    $args = [];
+                    while ($arg = $shift()) {
+                        $args[] = $arg;
+                    }
+
+                    $data = iter\map(
+                        function ($record) use ($args) {
+                            return iter\filter(
+                                function ($value, $key) use ($args) {
+                                    return in_array($key, $args);
+                                },
+                                $record
+                            )->toArray();
+                        },
+                        $data
+                    );
+                    break;
                 case 'map':
                 case 'mapBy':
                     $func = new \ReflectionFunction("Zicht\\Itertools\\" . $instruction);
